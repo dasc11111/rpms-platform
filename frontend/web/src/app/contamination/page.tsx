f1ba843ed48913d0f245b605fa5935460f226180
dasc11111 <tm.diego.solis@gmail.com>
2026-07-22 17:13:16 -0400

    Create page.tsx

---
+++ frontend/web/src/app/contamination/page.tsx
@@ -0,0 +1,7 @@
+import { ContaminationApp } from "@/components/contamination/contamination-app";
+
+export const dynamic = "force-dynamic";
+
+export default function ContaminationPage() {
+    return <ContaminationApp />;
+}
