70bde2463d6d65b4901b514e804e04a922f01ad6
dasc11111 <tm.diego.solis@gmail.com>
2026-08-29 15:55:12 -0400

    Fase 24 Activimetro Fase A: pagina wrapper de ficha tecnica del equipo

---
+++ frontend/web/src/app/quality-control/activimetro/equipment/page.tsx
@@ -0,0 +1,7 @@
+import ActivimetroEquipmentApp from "@/components/quality-control/activimetro-equipment-app";
+
+export const dynamic = "force-dynamic";
+
+export default function ActivimetroEquipmentPage() {
+  return <ActivimetroEquipmentApp />;
+}
