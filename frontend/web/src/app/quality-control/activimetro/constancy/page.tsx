b8f700183510c11c84a32c33381c4012ebc64426
dasc11111 <tm.diego.solis@gmail.com>
2026-08-30 10:05:35 -0400

    Add ACTIV-06 page wrapper (constancy)

---
+++ frontend/web/src/app/quality-control/activimetro/constancy/page.tsx
@@ -0,0 +1,7 @@
+import ActivimetroConstancyApp from "@/components/quality-control/activimetro-constancy-app";
+
+export const dynamic = "force-dynamic";
+
+export default function ActivimetroConstancyPage() {
+  return <ActivimetroConstancyApp />;
+}
