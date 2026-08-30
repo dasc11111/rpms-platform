61920a49e751fbb4337915b876b193a94448b46d
dasc11111 <tm.diego.solis@gmail.com>
2026-08-30 10:33:42 -0400

    Add ACTIV-07 page wrapper (purity)

---
+++ frontend/web/src/app/quality-control/activimetro/purity/page.tsx
@@ -0,0 +1,7 @@
+import ActivimetroPurityApp from "@/components/quality-control/activimetro-purity-app";
+
+export const dynamic = "force-dynamic";
+
+export default function ActivimetroPurityPage() {
+  return <ActivimetroPurityApp />;
+}
